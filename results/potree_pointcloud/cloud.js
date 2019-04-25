7c60ad3ac6928a29d85e98fd72d0dd892418f92b
MarcosFMAndrade <morem5@hotmail.com>
2019-04-26 00:46:47 +0100

    WebODM results

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 12667788,
+    "boundingBox": {
+        "lx": 554034.9738055931,
+        "ly": 4505755.284444829,
+        "lz": -18.218592927367298,
+        "ux": 554444.839042641,
+        "uy": 4506165.149681877,
+        "uz": 391.6466441205285
+    },
+    "tightBoundingBox": {
+        "lx": 554034.97,
+        "ly": 4505755.28,
+        "lz": -18.22,
+        "ux": 554444.84,
+        "uy": 4506088.82,
+        "uz": 62.410000000000007
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.839629650115967,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
